5dab041b83fa331159722681a4f0ebb53a38d586
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2022-10-24 12:45:42 +0200

    Update repo.js

--- src/apps/application/repo.js
+++ src/apps/application/repo.js
@@ -21,7 +21,7 @@ exports.create = async (firstName, lastName, email, phoneNumber, address, dob, c
 // get all applications
 exports.getAll = async () => {
     try {
-        return await Application.find();
+        return await Application.find().sort([firstName,1]);
     } catch (error) {
         throw error;
     }
