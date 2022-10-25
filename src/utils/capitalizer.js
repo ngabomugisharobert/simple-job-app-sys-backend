f7effd05df4c2721625a673da1d763f48ff13231
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2022-10-25 11:44:43 +0200

    fixed repo to match the deployment environment

--- src/utils/capitalizer.js
+++ src/utils/capitalizer.js
@@ -1,4 +1,8 @@
 const capitalizeFirstLetter = (string) => {
+    if (string.length > 0) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    else
+        return string;
 }
 module.exports = capitalizeFirstLetter;
